6071af1df6d7af0e236b0030bb9fd226994b09f4
unknown <jpradas18@gmail.com>
2021-04-14 12:03:53 +0200

    Primeros pasos en React Native

---
+++ App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Mensaje de prueba</Text>
+      <Text>Buenas</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#f6c90e',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
