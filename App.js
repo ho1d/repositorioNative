6cb29073fca79d3e3ea62ff9ca4df22798ab171a
unknown <jpradas18@gmail.com>
2021-04-16 15:58:44 +0200

    Commit 02 - Componentes en React Native

--- App.js
+++ App.js
@@ -1,22 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View } from 'react-native';
+import Campobase from './componentes/CampobaseComponent';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Mensaje de prueba</Text>
-      <Text>Buenas</Text>
+    <View>
+      <Campobase/>
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#f6c90e',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
